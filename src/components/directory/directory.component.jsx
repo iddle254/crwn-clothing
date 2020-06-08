b0c891fac4ed40c62e662bb9f83f65397210945c
Charles <charlesiddle99@gmail.com>
2020-06-09 00:31:01 +0300

    ch-added-routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({id, imageUrl, title, size})=>(
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...otherSectionProps})=>(
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         )
