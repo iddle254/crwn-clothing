8fa217278293903cc7d8c9ec0375560606dd12ac
Charles <charlesiddle99@gmail.com>
2020-10-02 09:14:10 +0300

    modified the cart icon and the menu item component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,68 @@
-import React from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {setCurrentUser} from './redux/user/user.actions';
-import './App.css';
-import Homepage from './pages/homepage/homepage.component';
-import Shoppage from './pages/shoppage/shoppage.component';
-import SigninandSignup from './pages/signin-and-signup/signinandsignup.component';
-import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import "./App.css";
+import Homepage from "./pages/homepage/homepage.component";
+import Shoppage from "./pages/shoppage/shoppage.component";
+import SigninandSignup from "./pages/signin-and-signup/signinandsignup.component";
+import Header from "./components/header/header.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  
-
   unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    const {setCurrentUser} = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
-      if(userAuth){
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapshot=>{
+        userRef.onSnapshot((snapshot) => {
           setCurrentUser({
             currentUser: {
               id: snapshot.id,
-              ...snapshot.data()
-            }
+              ...snapshot.data(),
+            },
           });
           // console.log(this.state);
-        }
-
-    );
+        });
       }
       setCurrentUser({
-        userAuth
+        userAuth,
       });
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  render(){
+  render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <Switch>
-        <Route exact path='/' component={Homepage}/>
-          <Route exact path='/shop' component={Shoppage}/>
-          <Route exact path='/signin' render={()=>this.props.currentUser? (<Redirect to='/' />): (<SigninandSignup/>)
-          } />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={Shoppage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SigninandSignup />
+            }
+          />
         </Switch>
-        
       </div>
     );
   }
-
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import {withRouter} from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)} >
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
