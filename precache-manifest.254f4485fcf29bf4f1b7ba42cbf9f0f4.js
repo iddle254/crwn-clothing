22a0e2f1a37d93d4a6dee50c9e69e05a3eb83bd4
Moss <charlesiddle99@gmail.com>
2022-09-02 23:28:24 +0300

    Updates

---
+++ precache-manifest.254f4485fcf29bf4f1b7ba42cbf9f0f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3ca5ca1346d6d103ade7c53b98cf7e",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "fb3c43bd7274b83c80d3",
+    "url": "/crwn-clothing/static/css/main.8183bc3b.chunk.css"
+  },
+  {
+    "revision": "dbae834c79e618fd2113",
+    "url": "/crwn-clothing/static/js/2.074b94aa.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-clothing/static/js/2.074b94aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3c43bd7274b83c80d3",
+    "url": "/crwn-clothing/static/js/main.16ef6422.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn-clothing/static/media/original.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
